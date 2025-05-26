cda012c3457b551015a562e178f8532d7ef00b31
farzas1 <eightfarzad@gmail.com>
2025-05-26 03:32:11 +0330

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,8 @@
+@echo off
+RDPWInst -i
+echo ______________________________________________________________
+echo.
+echo You can check RDP functionality with RDPCheck program.
+echo Also you can configure advanced settings with RDPConf program.
+echo.
+pause
